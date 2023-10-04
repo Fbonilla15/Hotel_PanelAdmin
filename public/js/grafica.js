a256e8efd4d0e532416ef1a92ec966f4a76841d2
Fbonilla15 <favio7155@gmail.com>
2023-10-04 12:55:48 -0500

    commit 3

--- public/js/grafica.js
+++ public/js/grafica.js
@@ -1,7 +1,9 @@
-let data1 = ['Disponibles', 'Ocupadas']
-let data2 = [150,76];
-
+let data1 = ['Estandar      ','Junior    ', 'Suite  ',' ']
+let data2 = [50,50,50,0]
+let data3 = ['Sencillas','Dobles','Triples','Cuadruples']
+let data4 = [45,43,30,32];
 const graf = document.getElementById('migrafica1').getContext('2d');
+const graf2 = document.getElementById('migrafica2').getContext('2d');
 const migrafica2 = new Chart(graf,{
     type: 'doughnut',
     data: {
@@ -10,13 +12,35 @@ const migrafica2 = new Chart(graf,{
       label: 'Habitaciones',
       data: data2,
         backgroundColor: [
-            '#16a34a',
-            '#dc2626'
+            '#33FFF0',
+            '#FF3371',
+            '#33FF3F',
+            '#FFFFFF'
         ],
         borderColor: [
-            '#312e81',
-            '#312e81'
+            
+            '#FFFFFF'
         ],
       hoverOffset: 4
     }]
+}});
+
+const migrafica3 = new Chart(graf2,{
+  type: 'doughnut',
+  data: {
+  labels: data3,
+  datasets: [{
+    label: 'Habitaciones',
+    data: data4,
+      backgroundColor: [
+        '#33FFF0',
+        '#FF3371',
+        '#33FF3F',
+        '#FF8AF4'
+      ],
+      borderColor: [
+        '#FFFFFF'
+      ],
+    hoverOffset: 4
+  }]
 }});
